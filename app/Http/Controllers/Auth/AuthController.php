25fbad9bbd5d82472e1d9a0f76d8bd3c350f38c0
codybuell <cody@codybuell.com>
2015-03-11 16:28:26 -0400

    Login and redirect path overrides

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,32 +7,46 @@ use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Registration & Login Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. By default, this controller uses
-	| a simple trait to add these behaviors. Why don't you explore it?
-	|
-	*/
+  /*
+  |--------------------------------------------------------------------------
+  | Overrides
+  |--------------------------------------------------------------------------
+  |
+  | Override default redirect location from `home` to `admin` and login url
+  | from auth/login to login.
+  |
+  */
 
-	use AuthenticatesAndRegistersUsers;
+  protected $loginPath    = 'login';
+  protected $redirectPath = 'admin';
 
-	/**
-	 * Create a new authentication controller instance.
-	 *
-	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
-	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
-	 * @return void
-	 */
-	public function __construct(Guard $auth, Registrar $registrar)
-	{
-		$this->auth = $auth;
-		$this->registrar = $registrar;
+  /*
+  |--------------------------------------------------------------------------
+  | Registration & Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles the registration of new users, as well as the
+  | authentication of existing users. By default, this controller uses
+  | a simple trait to add these behaviors. Why don't you explore it?
+  |
+  */
 
-		$this->middleware('guest', ['except' => 'getLogout']);
-	}
+  use AuthenticatesAndRegistersUsers;
+
+  /*
+  | Create a new authentication controller instance.
+  |
+  | @param  \Illuminate\Contracts\Auth\Guard  $auth
+  | @param  \Illuminate\Contracts\Auth\Registrar  $registrar
+  | @return void
+  */
+
+  public function __construct(Guard $auth, Registrar $registrar)
+  {
+    $this->auth = $auth;
+    $this->registrar = $registrar;
+
+    $this->middleware('guest', ['except' => 'getLogout']);
+  }
 
 }
