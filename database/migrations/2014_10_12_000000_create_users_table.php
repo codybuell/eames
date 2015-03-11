9f14916c30e42977b7d7897acc0640243297caf3
codybuell <cody@codybuell.com>
2015-03-10 23:30:53 -0400

    Updates to user authentication routes and table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration {
 			$table->increments('id');
 			$table->integer('role_id');                             // id of the users role, sets their access levels
 			$table->boolean('active');                              // enable / disable system access
-			$table->string('username')->unique();                   // users unique username
+			$table->string('name')->unique();                       // users unique username
 			$table->string('email')->unique();                      // users unique email address
 			$table->string('password', 60);                         // users password encrypted
 			$table->rememberToken();                                // remember token for password resets
