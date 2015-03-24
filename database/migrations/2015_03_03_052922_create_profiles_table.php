c92c4dee2df226956a30b24b6856ec9f60702847
codybuell <cody@codybuell.com>
2015-03-23 22:15:19 -0400

    Replace tabs with double spaces

--- database/migrations/2015_03_03_052922_create_profiles_table.php
+++ database/migrations/2015_03_03_052922_create_profiles_table.php
@@ -5,43 +5,43 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProfilesTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('profiles', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('user_id');                             // associated user
-			$table->integer('manager_id')->nullable();              // managers user id
-			$table->string('photo')->nullable();                    // url for user photo
-			$table->string('first_name')->nullable();               // users first name
-			$table->string('last_name')->nullable();                // users last name
-			$table->string('title')->nullable();                    // employment title / position
-			$table->string('phone_work_office')->nullable();        // work office phone number
-			$table->string('phone_work_mobile')->nullable();        // work cell phone number
-			$table->string('phone_personal_home')->nullable();      // personal home phone number
-			$table->string('phone_personal_mobile')->nullable();    // personal cell phone number
-			$table->string('office_location')->nullable();          // office location
-			$table->longText('notes')->nullable();                  // notes about the user (visible only to admin)
-			$table->integer('created_by');
-			$table->integer('updated_by');
-			$table->timestamps();
-			$table->dateTime('ping');
-		});
-	}
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('profiles', function(Blueprint $table)
+    {
+      $table->increments('id');
+      $table->integer('user_id');                             // associated user
+      $table->integer('manager_id')->nullable();              // managers user id
+      $table->string('photo')->nullable();                    // url for user photo
+      $table->string('first_name')->nullable();               // users first name
+      $table->string('last_name')->nullable();                // users last name
+      $table->string('title')->nullable();                    // employment title / position
+      $table->string('phone_work_office')->nullable();        // work office phone number
+      $table->string('phone_work_mobile')->nullable();        // work cell phone number
+      $table->string('phone_personal_home')->nullable();      // personal home phone number
+      $table->string('phone_personal_mobile')->nullable();    // personal cell phone number
+      $table->string('office_location')->nullable();          // office location
+      $table->longText('notes')->nullable();                  // notes about the user (visible only to admin)
+      $table->integer('created_by');
+      $table->integer('updated_by');
+      $table->timestamps();
+      $table->dateTime('ping');
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('profiles');
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('profiles');
+  }
 
 }
